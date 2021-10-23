f6a278bb1645c7e6c12e9da5bde88fa121cd15a0
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-10-23 19:16:49 +0200

    Create formula.swift

---
+++ formula.swift
@@ -0,0 +1,23 @@
+//
+//  Simple Calculator (formula)
+//
+//  Created by deathlezz on 23/10/2021.
+//
+
+// function accepting two parameters
+func calculate(_ firstNumber: Int, _ secondNumber: Int) {
+    // multiply
+    print(firstNumber * secondNumber)
+    // divide
+    print(firstNumber / secondNumber)
+    // add
+    print(firstNumber + secondNumber)
+    // subtract
+    print(firstNumber - secondNumber)
+}
+// call the function
+calculate(10, 5)
+// 50
+// 2
+// 15
+// 5
